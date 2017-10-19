12d666788b0d1e6e11e1f610e04e5ef2250c435c
Julien Molina <julien@molina.pro>
2017-10-19 11:19:46 +0200

    Fix Issue #7
    Make path absolute when SetPath is called

---
+++ Cake.XComponent.Test/XcStudioAliasesTest.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Cake.Core;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Cake.XComponent.Test
+{
+    [TestFixture]
+    public class XcStudioAliasesTest : XComponentTestBase
+    {
+        [TearDown]
+        public void TearDown()
+        {
+            XcStudio.XcStudioPath = null;
+        }
+
+        [TestCase("XCStudio.exe")]
+        [TestCase(@"subFolder\XCStudio.exe")]
+        [TestCase(@"..\subFolder\XCStudio.exe")]
+        public void TestSetXcStudioPath(string path)
+        {
+            var cakeContext = Substitute.For<ICakeContext>();
+            cakeContext.SetXcStudioPath(path);
+            Assert.AreEqual(Path.GetFullPath(path), XcStudio.XcStudioPath);
+        }
+    }
+}
